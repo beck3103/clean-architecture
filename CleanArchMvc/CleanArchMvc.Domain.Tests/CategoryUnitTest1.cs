53b91eeb28da75e639e2e863a4536796aabfeb27
Marlon da Silva Beck <marlon.beck@havan.com.br>
2023-01-18 20:47:40 -0300

    Iniciando projeto de Clean Archtecture

---
+++ CleanArchMvc/CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
@@ -0,0 +1,54 @@
+using Xunit;
+using FluentAssertions;
+using CleanArchMvc.Domain.Entities;
+using System;
+
+namespace CleanArchMvc.Domain.Tests
+{
+    public class CategoryUnitTest1
+    {
+        [Fact(DisplayName = "Create category with Valid State")]
+        public void CreateCategory_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Category(1, "Category Name");
+            action.Should()
+                .NotThrow<Validation.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create category with Invalid Id")]
+        public void CreateCategory_NegativeIdValue_DomainExceptionInvalid()
+        {
+            Action action = () => new Category(-1, "Category Name");
+            action.Should()
+                .Throw<Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value");
+        }
+
+        [Fact(DisplayName = "Create category with name too short")]
+        public void CreateCategory_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Category(1, "Ca");
+            action.Should()
+                .Throw<Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short, minimum 3 characters");
+        }
+
+        [Fact(DisplayName = "Create category with Invalid Id")]
+        public void CreateCategory_MissingNameValue_DomainExceptionRequiredName()
+        {
+            Action action = () => new Category(1, "");
+            action.Should()
+                .Throw<Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Name. Name is required");
+        }
+
+        [Fact(DisplayName = "Create category with null name")]
+        public void CreateCategory_NullNameValue_DomainExceptionRequiredName()
+        {
+            Action action = () => new Category(1, "");
+            action.Should()
+                .Throw<Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Name. Name is required");
+        }
+    }
+}
---
+++ CleanArchMvc/CleanArchMvc.Domain/Validation/DomainExceptionValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Validation
+{
+    public class DomainExceptionValidation : Exception
+    {
+        public DomainExceptionValidation(string erro) :base(erro)
+        {
+
+        }
+
+        public static void When(bool hasError, string error)
+        {
+            if (hasError)
+                throw new DomainExceptionValidation(error);
+
+        }
+    }
+}
